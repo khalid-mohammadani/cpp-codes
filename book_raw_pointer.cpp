2a463b45ef93cea313cc3970bd919b8a7e811294
khalid-mohammadani <khalid.mohammadani@gmail.com>
2025-05-07 19:51:32 +0200

    Add Book class using raw pointer

---
+++ book_raw_pointer.cpp
@@ -0,0 +1,35 @@
+// [Constructor] Book created
+// Title: The C++ Programming Language
+// Author: Bjarne Stroustrup
+// [Destructor] Book deleted
+
+#include<iostream>
+
+using namespace std;
+
+class Book
+{
+private:
+    string title,author;
+public:
+    Book(){ 
+        title="The C++ Programming Language";
+        author="Hunt";
+        cout<<"[Constructor] Book created\n";
+        cout<<"Book title "<<title<<"\n";
+        cout<<"Book author "<<author<<"\n";
+
+    }
+    ~Book(){
+        cout << "[Destructor] Book deleted\n";
+   }
+};
+
+int main()
+{
+    Book b1;                //b1 object is automatically deleted by C++   
+    Book* b2 = new Book();
+
+    delete b2;              //b2 object is deleted by us  
+    return 0;
+}
